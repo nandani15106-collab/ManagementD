a1d930b9669af7c3f7e350e35cb6152ee30a634f
unknown <Nandini>
2026-01-14 09:43:14 +0530

    Add implementation of the linear search algorithm

--- Method.java
+++ Method.java
@@ -1,18 +1,38 @@
-import java.util.Scanner;
+//  @SuppressWarnings("ConvertToTryWithResources")
+//  public static void visite( ) {
+//     Scanner Sc = new Scanner(System.in);
+//     int a = Sc.nextInt();
+//     int b = Sc.nextInt();
+//     int  sum = a+b;
+//     System.out.println("Calculate sum =" + sum);
+//     Sc.close();
+//  }   
+//    public static void main(String[] args) {
+//        visite();
+      
+//    }
+    
+//}
 
 class Method {
-  @SuppressWarnings("ConvertToTryWithResources")
-  public static void visite( ) {
-     Scanner Sc = new Scanner(System.in);
-     int a = Sc.nextInt();
-     int b = Sc.nextInt();
-     int  sum = a+b;
-     System.out.println("Calculate sum =" + sum);
-     Sc.close();
-  }   
-    public static void main(String[] args) {
-        visite();
-      
+    public static int Home(int locker[], int key) {
+       for (int i = 0; i < locker.length; i++) {
+          if (locker[i] == key) {
+            return i;
+           }
+       }
+      return -1;
     }
-    
-}
+
+   public static void main(String[] args) {
+       int locker[]= { 2, 3, 8, 9, 12, 15, 24, 35,};
+        int key = 2;
+
+     int found = Home( locker, key);
+      
+     if (found == -1) {
+       System.out.println("key is not  found");
+     } else {
+       System.out.println("found that  ="+ found);
+     }
+   }}
--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 This is my first Git Repository.
 <br>
 Author - Nandini nirmal
+ <br>
+ Add documentation and code for linear search
+
+This commit includes the code for the linear search algorithm, along with necessary comments and a brief explanation in file. The implementation ensures all elements are checked sequentially.
 
 
